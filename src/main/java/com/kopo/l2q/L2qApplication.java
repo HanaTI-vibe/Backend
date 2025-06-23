618906ee818a6e0f9b78b30ed6c6b4e56572d6e3
fewolee <chulwoo25@naver.com>
2025-06-23 11:50:31 +0900

    first commit

---
+++ src/main/java/com/kopo/l2q/L2qApplication.java
@@ -0,0 +1,13 @@
+package com.kopo.l2q;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2qApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2qApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kopo/l2q/L2qApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopo.l2q;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2qApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
